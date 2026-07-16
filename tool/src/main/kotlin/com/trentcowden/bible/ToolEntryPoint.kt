19ebc7b48356555079050c4c19b3fd20ad447ef0
Trent Cowden <trent@trentcowden.com>
2026-07-15 20:56:05 -0700

    chore: remove push notification stuff

--- tool/src/main/kotlin/com/trentcowden/bible/ToolEntryPoint.kt
+++ tool/src/main/kotlin/com/trentcowden/bible/ToolEntryPoint.kt
@@ -1,6 +1,5 @@
-package com.thelightphone.sample
+package com.trentcowden.bible
 
-import android.util.Log
 import com.thelightphone.sdk.EntryPoint
 import com.thelightphone.sdk.LightEntryPoint
 import com.thelightphone.sdk.shared.LightServerData
@@ -12,15 +11,6 @@ object ToolEntryPoint : LightEntryPoint {
     override suspend fun onToolCreate(
         serverData: StateFlow<LightServerData?>,
     ) {
-        serverData.collect {
-            // this is where you'd send push credentials up to your app server
-            Log.d("ToolEntryPoint", "Current LightOS registration data: $it")
-        }
-    }
-
-    override suspend fun onPushNotification(
-        data: ByteArray,
-    ) {
-        Log.d("ToolEntryPoint", "received push notification: $data")
+        serverData.collect {}
     }
 }
